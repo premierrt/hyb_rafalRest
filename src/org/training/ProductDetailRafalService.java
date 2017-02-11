e71b77791243bdd396b81ae52fd5261f2069af36
rafal_robota <rafal.tyszewski@play.pl>
2017-02-11 20:21:45 +0100

    interfejs1

---
+++ src/org/training/ProductDetailRafalService.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+package org.training;
+
+import de.hybris.platform.core.model.product.ProductModel;
+
+
+/**
+ * @author tyszewr
+ *
+ */
+public interface ProductDetailRafalService
+{
+	ProductModel getProductDetails();
+}
---
+++ src/org/training/imp/DefaultProductDetailRafalService.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.training.imp;
+
+import de.hybris.platform.core.model.product.ProductModel;
+
+import org.training.ProductDetailRafalService;
+
+/**
+ * @author tyszewr
+ *
+ */
+public class DefaultProductDetailRafalService implements ProductDetailRafalService
+{
+
+	/* (non-Javadoc)
+	 * @see org.training.ProductDetailRafalService#getProductDetails()
+	 */
+	@Override
+	public ProductModel getProductDetails()
+	{
+		// YTODO Auto-generated method stub
+		return null;
+	}
+
+}
