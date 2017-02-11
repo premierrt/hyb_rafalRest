627bee406dce79baad86dee7b33126a8e0f45d50
rafal_robota <rafal.tyszewski@play.pl>
2017-02-11 20:36:14 +0100

    dao

---
+++ src/org/training/dao/ProductDetailRafalDao.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package org.training.dao;
+
+import de.hybris.platform.core.model.product.ProductModel;
+
+import java.util.List;
+
+
+/**
+ * @author tyszewr
+ *
+ */
+public interface ProductDetailRafalDao
+{
+	List<ProductModel> getProductDetails();
+}
---
+++ src/org/training/dao/imp/DefaultProductDetailRafalDao.java
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+package org.training.dao.imp;
+
+import de.hybris.platform.core.model.product.ProductModel;
+
+import java.util.List;
+
+import org.training.dao.ProductDetailRafalDao;
+
+
+/**
+ * @author tyszewr
+ *
+ */
+public class DefaultProductDetailRafalDao implements ProductDetailRafalDao
+{
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.training.dao.ProductDetailRafalDao#getProductDetails()
+	 */
+	@Override
+	public List<ProductModel> getProductDetails()
+	{
+
+
+
+		return null;
+	}
+
+}
