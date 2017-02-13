230c572ad91af0f4318ade43da166e2e0ef22413
rafal_robota <rafal.tyszewski@play.pl>
2017-02-13 15:47:31 +0100

    test 1

---
+++ testsrc/org/training/DefaultRafalServiceTest.java
@@ -0,0 +1,54 @@
+/**
+ *
+ */
+package org.training;
+
+import static org.junit.Assert.assertEquals;
+
+import de.hybris.platform.core.model.product.ProductModel;
+import de.hybris.platform.servicelayer.ServicelayerTransactionalTest;
+import de.hybris.platform.servicelayer.model.ModelService;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+
+/**
+ * @author tyszewr
+ *
+ */
+
+public class DefaultRafalServiceTest extends ServicelayerTransactionalTest
+{
+
+	@Resource(name = "defaultProductDetailService")
+	private ProductDetailRafalService productService;
+
+	@Resource
+	private ModelService modelService;
+
+	private ProductModel productModel;
+
+	@Before
+	public void setUp()
+	{
+		productModel = new ProductModel();
+		productModel.setDescription("to jest nowy produkt describtion");
+
+	}
+
+	@Test
+	public void testService()
+	{
+		modelService.save(productModel);
+		final List<ProductModel> pms = productService.getProductDetails();
+		assertEquals(1, pms.size());
+	}
+
+}
